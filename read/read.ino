8558efb8614cf96c91f822d1079b56480a4d0508
MrMatrix <trimantuteja@gmail.com>
2024-01-08 19:43:12 +0530

    Added barometer support with basic smoothing

--- read/read.ino
+++ read/read.ino
@@ -1,8 +1,10 @@
 #include <Wire.h>
 #include <MPU6050.h>
 #include "Servo.h"
+#include <MS5611.h>
 
 MPU6050 mpu;
+MS5611 ms5611;
 Servo elevatorL;
 Servo elevatorR;
 Servo rudder;
@@ -27,9 +29,11 @@ double integral = 0.0;  // Changing integral gain
 
 // Modular control
 int dir = 1;
+float smoothingFactor = 0.05;
 
 // Stores error in prior run
 double previousError = [0,0,0];
+double altitude = 0.0;
 
 // Sensor Errors
 double accXError = 0.0;
@@ -37,6 +41,7 @@ double accYError = 0.0;
 double gyroXError = 0.0;
 double gyroYError = 0.0;
 double gyroZError = 0.0;
+double seaPressure = 0.0;
 
 // angle
 double accX, accY, accZ;
@@ -54,8 +59,19 @@ void setup() {
   elevatorR.attach(elevatorPinR);
   rudder.attach(rudderPin);
 
+  // Setting up the barometer MS5611
+  while (!ms5611.begin())
+  {
+    // Throws error if not detected
+    Serial.println("Could not find a valid MS5611 sensor, check wiring!");
+    delay(500);
+  }
+
   // Calibrating and finding the intial errors in the sensor outputs
   calibrate();
+
+  // Setting initial value for smoothAlt based on pressure at time of launch
+  altitude = 44330 * (1 - pow(seaPressure / 101325, 0.1903));
 }
 
 void loop() {
@@ -74,6 +90,10 @@ void loop() {
   double correctedX = correctedAngle(gyroX, accX, accY, accZ, 0);
   double correctedY = correctedAngle(gyroY, accX, accY, accZ, 1);
 
+  // Calculate new altitude and calls a smoothning function with previous altitude and 
+  double newAlt = 44330 * (1 - pow(ms5611.getPressure() / 101325, 0.1903));
+  altitude = ema(altitude, newAlt);
+
   // Calculating PID values in X and Y axis
   double XPID = findPID(correctedX, 0);
   double YPID = findPID(correctedY, 1);
@@ -89,8 +109,8 @@ void loop() {
 
 double correctedAngle(int gyro, int accX, int accY, int accZ, int axis) {
   // Calculating the angle of drone using sensor data from accelerometer
-  double accAngleX = (atan(accY / sqrt(pow(accX, 2) + pow(accZ, 2))) * 180 / PI) - XError; 
-  double accAngleY = (atan(-1 * accX / sqrt(pow(accY, 2) + pow(accZ, 2))) * 180 / PI) + YError; //correct the errors
+  double accAngleX = (atan(accY / sqrt(pow(accX, 2) + pow(accZ, 2))) * 180 / PI) - accXError; 
+  double accAngleY = (atan(-1 * accX / sqrt(pow(accY, 2) + pow(accZ, 2))) * 180 / PI) + accYError; //correct the errors
 
   // Storing in an array to make the code modular
   double accAngle = [accAngleX, accAngleY];
@@ -127,6 +147,12 @@ void setMotorAngle(Servo motor, int angle) {
   Serial.println("Setting Motor Angle: " + String(angle));
 }
 
+double ema(double oldValue, double newValue) {
+  // have a smoothing factor to define the ratio to take
+  double Value = (1.0 - smoothingFactor) * oldValue + smoothingFactor * newValue;
+  return Value;
+}
+
 void calibrate () {
 
   while (c < 200) {
@@ -161,4 +187,12 @@ void calibrate () {
   gyroXError = gyroXError / 200;
   gyroYError = gyroYError / 200;
   gyroZError = gyroZError / 200;
+
+  c = 0;
+  // Read MS5611 200 times
+  while (c < 200) {
+    seaPressure += ms5611.getPressure();
+  }
+  seaPressure = seaPressure / 200; 
+
 }
